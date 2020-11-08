82e3fe6f98628e6de8228cc4d47d75932dd5ef0b
whodanyalahmed <daniahmedkhatri@gmail.com>
2020-11-08 16:21:45 +0500

    learning lifecycle and reduc

--- todos/src/container/About.js
+++ todos/src/container/About.js
@@ -1,10 +1,14 @@
 import React ,{Component} from 'react'
-
+import { Link } from "react-router-dom";
 
 class About extends Component{
     render(){
         return (
+            <div>
             <h1>About page</h1>
+            <br/>
+            <Link to="/home"> Home</Link>
+            </div>
         )
     }
 }
--- todos/src/container/Home.js
+++ todos/src/container/Home.js
@@ -1,13 +1,67 @@
 import React ,{Component} from 'react'
 
+import {Link} from 'react-router-dom';
+
 
 class Home extends Component{
+    constructor(){
+        super()
+        this.state = {
+            count: 0
+        }
+        console.log('constructor');
+    }
+    ZeroLess(){
+        if(this.state.count === 0){
+            return <h3>Cant decrease zero</h3>
+        }
+        else{
+            return <h3></h3>
+        }
+    }
+    CheckNDecrease(){
+        if(this.state.count >= 1){
+            this.setState({count: this.state.count -1})
+        }
+        else{
+            this.setState({
+                count: this.state.count
+            })
+            const element = <h4>Cant decrease zero</h4>;
+            this.render(element, document.getElementById('show'));
+            
+            console.log("Cant decrease zero")
+            // return element;
+        }
+    }
+    // static getDerivedStateFromProps(){
+    //     console.log('getDrivedStatefromprops')
+    //     return{
+    //         count: 5
+    //     }
+
+    // }
+
+    componentDidMount(){
+        console.log("ComponentDidMount bish")
+    }
     render(){
+        console.log("render working bitch")
         return (
-            <div>
+            <div style={{textAlign: 'center'}}>
             <h1>Home page</h1>
+            
+            <div id="show">
+            </div>
+        <h3>{this.state.count}</h3>
+        <button onClick={()=> this.setState({count: this.state.count + 1})}>Increament</button>
+        <button onClick={()=> this.CheckNDecrease()}>Decreament</button>
+
+   
             <br/>
-            <a href="/about" >About</a>
+            {/* we are not going to use that shit bruh,,,,, */}
+            {/* <a href="/about" >About</a> */}
+            <Link to="/about">About</Link>
             </div>
         )
     }
